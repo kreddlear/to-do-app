1f07c203c2a060cd4221a4a5ce909316881fd3ab
kreddlear <kredlear@gmail.com>
2015-11-18 21:08:30 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,11 +4,3 @@
 var app = angular.module('toDo', [
   'firebase',
 ]);
-/*
- .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {
-	  templateUrl: 'index.html',
-	  controller: 'toDoController'
-	  });
-}]);
-*/
